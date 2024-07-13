2130ca134147d9c87381d1157d53e0ad387a7cb3
Hansanghyeon <999@hyeon.pro>
2024-07-13 16:16:40 +0000

    feat: tailwindcss 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return (
+    <main>
+      <div className="h-[100dvh] grid place-content-center">
+        <article className="prose">
+          <h1>훈민정음</h1>
+          <p>
+            나랏말싸미 듕귁에 달아문자와로 서르 사맛디 아니할쎄 이런 전차로 어린
+            백셩이 니르고져 홇베이셔도 마참네 제 뜨들 시러펴디 몯핧 노미하니아
+            내 이랄 윙하야 어엿비너겨 새로 스믈 여듫 짜랄 맹가노니사람마다 해여
+            수비니겨 날로 쑤메 뻔한킈 하고져 할따라미니라
+          </p>
+        </article>
+      </div>
+    </main>
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,37 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./src/**/*.tsx', './src/**/*.css'],
+  content: ['./src/**/*.{tsx,css}'],
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '1rem',
+        lg: '2rem',
+        xl: '2rem',
+        '2xl': '2rem',
+      },
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        sans: '"Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif'.split(
+          ', ',
+        ),
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+  ],
 }
