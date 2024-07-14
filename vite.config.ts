cbe1232bdd44c994405b44a04b321a6868b73404
Hansanghyeon <999@hyeon.pro>
2024-07-14 05:40:59 +0000

    feat: react-router-dom alias 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import generouted from '@generouted/react-router/plugin'
@@ -5,4 +6,12 @@ import generouted from '@generouted/react-router/plugin'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), generouted()],
+  resolve: {
+    alias: [
+      {
+        find: '~',
+        replacement: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
 })
