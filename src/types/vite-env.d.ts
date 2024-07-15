1c701bd47b552d191fe6743a61dc655b407afabd
Hansanghyeon <999@hyeon.pro>
2024-07-15 03:46:22 +0000

    feat: axios 인스턴스 생성

--- src/types/vite-env.d.ts
+++ src/types/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_NODE_ENV: 'development' | 'production' | 'staging' | 'local'
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
