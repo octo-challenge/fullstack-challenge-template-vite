66e9e1e33362f1cb9a2e1e9df5ff50584c3d9a58
Hansanghyeon <999@hyeon.pro>
2024-07-19 07:42:17 +0000

    feat: jotai 셋업

--- src/app/provider/app-provider.tsx
+++ src/app/provider/app-provider.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { pipe, flow } from 'fp-ts/function'
 import { BrwoserRouterEventContainer } from '~/app/provider/react-router-dom'
 import { QueryClientProvider } from './tanstack-query'
+import { JotaiProvider } from './jotai'
 
 // 각 Provider를 래핑하는 함수
 const wrap =
@@ -14,6 +15,7 @@ const AppProvider =
     const ComposedProviders = ({ children }: { children: React.ReactNode }) =>
       pipe(
         children,
+        wrap(JotaiProvider),
         wrap(QueryClientProvider),
         wrap(BrwoserRouterEventContainer),
       )
