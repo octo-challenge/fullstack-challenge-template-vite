7fe3bfeaa920d6a64404e88c391588cf74801fa0
Hansanghyeon <999@hyeon.pro>
2024-07-21 08:36:37 +0000

    feat: radix 모달열렸을때 포지션 수정

--- src/shared/components/gnb/gnb.tsx
+++ src/shared/components/gnb/gnb.tsx
@@ -26,7 +26,7 @@ import { useAuth } from '~/shared/hooks/use-auth'
 
 export function GNB() {
   const auth = useAuth().value
-  const headerRef = React.useRef<HTMLElement>(null)
+  const headerRef = React.useRef<HTMLDivElement>(null)
   React.useEffect(() => {
     const header = headerRef.current
     if (!header) {
@@ -36,92 +36,94 @@ export function GNB() {
     headroom.init()
   }, [])
   return (
-    <header
-      ref={headerRef}
-      className="fixed top-0 flex h-[var(--gnb-h)] w-full shrink-0 items-center px-4 md:px-6"
-    >
-      <Link to="/" className="text-8 mr-6 flex items-center">
-        <LuMountain className="h-auto w-8" />
-        <span className="sr-only">fullstack-challenge-template-vite</span>
-      </Link>
-      <nav className="ml-auto flex items-center gap-4 sm:gap-6">
-        <NavigationMenu>
-          <NavigationMenuList>
-            <NavigationMenuLink asChild>
-              <Link
-                to="/"
-                className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-              >
-                Home
-              </Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link
-                to="/"
-                className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-              >
-                About
-              </Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link
-                to="/"
-                className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-              >
-                Services
-              </Link>
-            </NavigationMenuLink>
-            <NavigationMenuLink asChild>
-              <Link
-                to="/account"
-                className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
-              >
-                account
-              </Link>
-            </NavigationMenuLink>
-          </NavigationMenuList>
-        </NavigationMenu>
-        {auth?.accessToken ? (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <button type="button" className="focus:outline-none">
-                <Avatar className="h-9 w-9">
-                  <AvatarImage src="/placeholder-user.jpg" />
-                  <AvatarFallback>
-                    <IoPerson />
-                  </AvatarFallback>
-                  <span className="sr-only">Toggle user menu</span>
-                </Avatar>
-              </button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem>
-                <Link to="/" className="flex items-center gap-2">
-                  <div className="h-4 w-4" />
-                  <span>Account</span>
+    <header className="fixed top-0 w-full pr-[var(--removed-body-scroll-bar-size)]">
+      <div
+        ref={headerRef}
+        className="flex h-[var(--gnb-h)] w-full shrink-0 items-center px-4 pr-[var(--fix-scrollbar-size)] md:px-6"
+      >
+        <Link to="/" className="text-8 mr-6 flex items-center">
+          <LuMountain className="h-auto w-8" />
+          <span className="sr-only">fullstack-challenge-template-vite</span>
+        </Link>
+        <nav className="ml-auto flex items-center gap-4 sm:gap-6">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuLink asChild>
+                <Link
+                  to="/"
+                  className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                >
+                  Home
                 </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link to="/" className="flex items-center gap-2">
-                  <div className="h-4 w-4" />
-                  <span>Settings</span>
+              </NavigationMenuLink>
+              <NavigationMenuLink asChild>
+                <Link
+                  to="/"
+                  className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                >
+                  About
                 </Link>
-              </DropdownMenuItem>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>
-                <Link to="/signout" className="flex items-center gap-2">
-                  <div className="h-4 w-4" />
-                  <span>Logout</span>
+              </NavigationMenuLink>
+              <NavigationMenuLink asChild>
+                <Link
+                  to="/"
+                  className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                >
+                  Services
                 </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        ) : (
-          <Button asChild>
-            <Link to="/signin">로그인</Link>
-          </Button>
-        )}
-      </nav>
+              </NavigationMenuLink>
+              <NavigationMenuLink asChild>
+                <Link
+                  to="/account"
+                  className="bg-background hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground data-[active]:bg-accent/50 data-[state=open]:bg-accent/50 group inline-flex h-9 w-max items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus:outline-none disabled:pointer-events-none disabled:opacity-50"
+                >
+                  account
+                </Link>
+              </NavigationMenuLink>
+            </NavigationMenuList>
+          </NavigationMenu>
+          {auth?.accessToken ? (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <button type="button" className="focus:outline-none">
+                  <Avatar className="h-9 w-9">
+                    <AvatarImage src="/placeholder-user.jpg" />
+                    <AvatarFallback>
+                      <IoPerson />
+                    </AvatarFallback>
+                    <span className="sr-only">Toggle user menu</span>
+                  </Avatar>
+                </button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem>
+                  <Link to="/" className="flex items-center gap-2">
+                    <div className="h-4 w-4" />
+                    <span>Account</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link to="/" className="flex items-center gap-2">
+                    <div className="h-4 w-4" />
+                    <span>Settings</span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link to="/signout" className="flex items-center gap-2">
+                    <div className="h-4 w-4" />
+                    <span>Logout</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          ) : (
+            <Button asChild>
+              <Link to="/signin">로그인</Link>
+            </Button>
+          )}
+        </nav>
+      </div>
     </header>
   )
 }
