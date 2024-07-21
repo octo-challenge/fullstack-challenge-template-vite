7614c25b5f768dbe19ee01a8ec5c503ecd6cf193
Hansanghyeon <999@hyeon.pro>
2024-07-21 07:52:16 +0000

    fix: signin 페이지 오타 수정

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -3,7 +3,7 @@ import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 export const Route = createFileRoute('/_auth')({
   beforeLoad: ({ location, context }) => {
     // 로그인 페이지로 이동하려하는데 인증된 상태라면 dashboard로 리다이렉트한다
-    if (location.pathname === '/singin' && context.auth.value) {
+    if (location.pathname === '/signin' && context.auth.value) {
       throw redirect({ to: '/dashboard' })
     }
     return null
