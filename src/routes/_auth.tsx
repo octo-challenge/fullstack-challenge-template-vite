dedbc8573cca21ce30b042a8b5e6883267969f63
Hansanghyeon <999@hyeon.pro>
2024-07-21 04:45:20 +0000

    chore: 사용하지 않는 코드 제거

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,7 +1,7 @@
 import { createFileRoute, Outlet, redirect } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_auth')({
-  beforeLoad: ({ location, context, ...p }) => {
+  beforeLoad: ({ location, context }) => {
     // 로그인 페이지로 이동하려하는데 인증된 상태라면 dashboard로 리다이렉트한다
     if (location.pathname === '/singin' && context.auth.value) {
       throw redirect({ to: '/dashboard' })
