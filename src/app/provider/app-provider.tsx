3263a3ccc8557281fe30baee00fed7521de77277
Hansanghyeon <999@hyeon.pro>
2024-09-16 08:39:33 +0000

    feat: remove jotai

--- src/app/provider/app-provider.tsx
+++ src/app/provider/app-provider.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { pipe } from 'fp-ts/function'
-import { QueryClientProvider } from './tanstack-query'
-import { JotaiProvider } from './jotai'
 import { TanstackRouterProvider } from './tanstack-router'
 
 // 각 Provider를 래핑하는 함수
@@ -13,12 +11,7 @@ const AppProvider =
   <P extends object>(WrappedComponent: React.ComponentType<P>) =>
   (props: P) => {
     const ComposedProviders = ({ children }: { children: React.ReactNode }) =>
-      pipe(
-        children,
-        wrap(JotaiProvider),
-        wrap(QueryClientProvider),
-        wrap(TanstackRouterProvider),
-      )
+      pipe(children, wrap(TanstackRouterProvider))
     return (
       <ComposedProviders>
         <WrappedComponent {...props} />
--- src/app/provider/jotai.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { useAtomsDevtools } from 'jotai-devtools'
-
-export function JotaiProvider({ children }: React.PropsWithChildren) {
-  useAtomsDevtools('fullstack-challenge-template-vite')
-  return children
-}
