6d4d6e7a7a20379163ca5f72d44f0a8690bb54c1
Hansanghyeon <999@hyeon.pro>
2024-09-16 09:38:29 +0000

    feat: api 요청에 인증키 포함

--- src/shared/api/axios.ts
+++ src/shared/api/axios.ts
@@ -1,10 +1,25 @@
 import axios from 'axios'
+import { AuthManager } from '../managers/auth'
 
 export function ApiClient() {
-  return axios.create({
+  const instance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
       'Content-Type': 'application/json',
     },
   })
+
+  instance.interceptors.request.use(async (request) => {
+    // 로그인 하는 요청에는 accessToken을 가져오지 않는다.
+    if (request.url === '/auth/sign-in') {
+      return request
+    }
+
+    const token = AuthManager.token
+
+    request.headers.Authorization = `Bearer ${token.accessToken}`
+    return request
+  })
+
+  return instance
 }
