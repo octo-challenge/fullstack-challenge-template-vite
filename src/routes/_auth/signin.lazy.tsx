82007ece5f4734ecccb43e1e03dd8a7982e0ad76
Hansanghyeon <999@hyeon.pro>
2024-09-16 04:43:40 +0000

    feat: sign-* auth 폴더로 이동

--- src/routes/_auth/signin.lazy.tsx
+++ src/routes/_auth/signin.lazy.tsx
@@ -4,7 +4,7 @@ import { Button } from '~/shared/components/ui/button'
 import { Input } from '~/shared/components/ui/input'
 import { FormProvider, useForm } from 'react-hook-form'
 import { useAuth } from '~/shared/hooks/use-auth'
-import { postPayloadSignInDto, postSignIn } from '~/api/sign-in'
+import { postPayloadSignInDto, postSignIn } from '~/api/auth/sign-in'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { toast } from 'react-toastify'
 import {
